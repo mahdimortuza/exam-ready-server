6474989371c3186b184275b932d7524d16cf8321
mahdimortuza <mahdi.webx@gmail.com>
2024-06-12 13:14:13 +0600

    authorization added

--- src/app/modules/user/user.constants.ts
+++ src/app/modules/user/user.constants.ts
@@ -1,1 +1,8 @@
 export const UserStatus = ['in-progress', 'blocked'];
+
+export const USER_ROLE = {
+  normalUser: 'normalUser',
+  student: 'student',
+  studentPlus: 'studentPlus',
+  admin: 'admin',
+} as const;
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { createStudentValidationSchema } from '../student/student.validation';
+import { USER_ROLE } from './user.constants';
 import { UserController } from './user.controller';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(createStudentValidationSchema),
   UserController.createStudent,
 );
