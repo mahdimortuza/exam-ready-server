7271d4a607979330040bce49eed99913912b05ed
mahdimortuza <mahdi.webx@gmail.com>
2024-10-31 16:34:11 +0600

    exam model updated

--- src/app/modules/exams/exam.interface.ts
+++ src/app/modules/exams/exam.interface.ts
@@ -1,7 +1,9 @@
+import { Types } from 'mongoose';
+
 export type TExam = {
   examName: string; // Title of the exam
   createdBy: string; // Email or identifier of the creator
-  questions: object[]; // Array of objects representing questions (flexible structure)
+  questions: Types.ObjectId[]; // Array of objects representing questions (flexible structure)
   status: 'upcoming' | 'ongoing' | 'finished'; // Status of the exam
   startTime: string; // Start time of the exam
   endTime: string; // End time of the exam
--- src/app/modules/exams/exam.model.ts
+++ src/app/modules/exams/exam.model.ts
@@ -1,4 +1,4 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, Types } from 'mongoose';
 import { TExam } from './exam.interface';
 
 const examSchema = new Schema<TExam>(
@@ -12,21 +12,21 @@ const examSchema = new Schema<TExam>(
       type: String,
       required: [true, 'Admin email is required'],
     },
-    // questions: {
-    //   type: [Types.ObjectId],
-    //   required: [true, 'Exam quizzes are required'],
-    //   ref: 'ExamQuiz',
-    //   validate: {
-    //     validator: function (value: Types.ObjectId[]) {
-    //       return value.length === 5; // Must have exactly 5 questions
-    //     },
-    //     message: 'Exactly 5 questions are required',
-    //   },
-    // },
     questions: {
-      type: [Schema.Types.Mixed], // Array of objects, allowing for flexible structure
-      required: true, // Ensure that questions are provided
+      type: [Types.ObjectId],
+      required: [true, 'Exam quizzes are required'],
+      ref: 'ExamQuiz',
+      validate: {
+        validator: function (value: Types.ObjectId[]) {
+          return value.length === 5; // Must have exactly 5 questions
+        },
+        message: 'Exactly 5 questions are required',
+      },
     },
+    // questions: {
+    //   type: [Schema.Types.Mixed], // Array of objects, allowing for flexible structure
+    //   required: true, // Ensure that questions are provided
+    // },
 
     status: {
       type: String,
