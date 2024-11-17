7dd139fb77623d2d3b92c31e7ef56dd9e25eb5df
mahdimortuza <mahdi.webx@gmail.com>
2024-11-17 12:34:33 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://exam-ready-client-latest.vercel.app'],
     methods: 'GET, POST, PUT, PATCH, DELETE',
     credentials: true,
   }),
